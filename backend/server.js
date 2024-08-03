b9f939093a2fdef0f93a65d435c18a089616dc1d
anjubora <ashishofficial026@gmail.com>
2024-08-03 22:56:22 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
@@ -10,9 +11,17 @@ import { app, server } from "./socket/socket.js"
 
 const PORT=process.env.PORT||5000
 dotenv.config();
+const __dirname = path.resolve();
+
 app.use(express.json());//to parse the incoming request with json payload(from req.body)
 app.use(cookieparser());// to parse the incoming cookie from req.cookies
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT,()=>{console.log(`server runing on port ${PORT}`) ,connectToMongoDB()})
