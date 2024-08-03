966baf622e7a5ca5e1f195ef35d606480b331cec
anjubora <ashishofficial026@gmail.com>
2024-08-04 00:00:55 +0530

    final update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     port:3000,
   proxy:{
-    "/api":{target:"https://chatapplication-5ep0.onrender.com/",},
+    "/api":{target:"https://chatapplication-5ep0.onrender.com",},
   },
 },
 
