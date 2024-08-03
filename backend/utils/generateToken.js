acbafba74ff28954825b8a9dc75c1f46e777f0d4
anjubora <ashishofficial026@gmail.com>
2024-08-03 23:16:54 +0530

    final update

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,8 @@ const token=jwt.sign({userId},process.env.JWT_SECRET,{ expiresIn:"15d"},);
 res.cookie("jwt",token,{  
     maxAge:15 * 24 * 60 * 60 * 1000,
     httpOnly:true,
-    sameSite:"strict",  
+    sameSite:"strict",
+    secure: process.env.NODE_ENV !== "development",  
 })
  }
  export default generateTokenAndSetCookie; 
